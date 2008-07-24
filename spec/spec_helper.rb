02eb5902c386458ce3e4d14a2aaff767d5067d4f
Samuel Quinn Slack <qslack@qslack.com>
2008-07-24 21:36:44 +0800

    updated specs to use more generic fixtures, etc.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,14 @@ Rhino::Base.connect("http://localhost:60010/api")
 class Page < Rhino::Base
   column_family :title
   column_family :contents
-  column_family :text
+  column_family :links
   column_family :meta#, :columns=>%w(keywords author encoding mime language)
+end
+
+page_key = 'example.com'
+page_data = {:contents=>"<h1>Welcome to Example Page</h1>",
+            :title=>'Example Page', :meta_author=>'John Smith'}
+unless page = Page.find(page_key) and page.data == page_data
+  puts "Creating mock Page with key='#{page_key}' and data #{page_data.inspect}"
+  Page.create(page_key, page_data)
 end
