c68db1dcb8280380646ae8bcbd33ceef8af09e95
Samuel Quinn Slack <qslack@qslack.com>
2008-07-25 17:08:47 +0800

    added jots example

---
+++ examples/jots.rb
@@ -0,0 +1,36 @@
+# schema: users(email:, jots:)
+# in the ruby shell: create 'users', 'email', 'jots'
+
+require 'lib/rhino'
+
+include Rhino::Debug
+
+Rhino::Base.connect("http://localhost:60010/api")
+
+class Jot < Rhino::PromotedColumnFamily
+  belongs_to :user
+end
+
+class User < Rhino::Base
+  column_family :email
+  column_family :jots
+  
+  has_many :jots, Jot
+end
+
+sqs = User.find('sqs') || 
+    ( User.create('sqs', :email=>'user@example.com') &&
+      sqs.set_attribute('jots:Flights', 'SQ27 EWR-SIN') &&
+      sqs.set_attribute('jots:Friends', 'Sally, Billy, Bob, John') )
+
+sqs.save
+
+
+u = User.find('sqs')
+puts "User inspect: #{u.inspect}"
+puts
+puts "Listing jots:"
+u.jots.keys.each do |jot_key|
+  puts "#{jot_key}: #{u.jots[jot_key].contents}"
+end
+puts
