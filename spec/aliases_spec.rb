509f1d23bfaa161ea241c66449968b76deffa9c6
Quinn Slack <qslack@qslack.com>
2008-07-30 23:12:32 +0800

    moved aliases-related specs to their own file and removed them from model_spec

---
+++ spec/aliases_spec.rb
@@ -0,0 +1,21 @@
+require File.dirname(__FILE__) + '/spec_helper.rb'
+
+describe "when using attribute aliases" do
+  it "should read the value of the target" do
+    @page = Page.new('some-page')
+    @page.meta_author = 'Alice'
+    @page.author.should == 'Alice'
+  end
+  
+  it "should set the value of the target" do
+    @page = Page.new('some-page')
+    @page.author = 'Cindy'
+    @page.meta_author.should == 'Cindy'
+  end
+  
+  it "should allow instantiation using attribute aliases" do
+    @page = Page.create('some-page', :author=>'Bob', :title=>'a title')
+    @page.meta_author.should == 'Bob'
+    @page.author.should == 'Bob'
+  end
+end
--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -246,26 +246,6 @@ describe Rhino::Model do
     it "should retrieve only the requested columns"
   end
   
-  describe "when using attribute aliases" do
-    it "should read the value of the target" do
-      @page = Page.new('some-page')
-      @page.meta_author = 'Alice'
-      @page.author.should == 'Alice'
-    end
-    
-    it "should set the value of the target" do
-      @page = Page.new('some-page')
-      @page.author = 'Cindy'
-      @page.meta_author.should == 'Cindy'
-    end
-    
-    it "should allow instantiation using attribute aliases" do
-      @page = Page.create('some-page', :author=>'Bob', :title=>'a title')
-      @page.meta_author.should == 'Bob'
-      @page.author.should == 'Bob'
-    end
-  end
-  
   describe "when testing for equality between two rows" do
     it "should find two rows equal if their key, data, and timestamp are the same" do
       Page.create('a', :title=>'b')
