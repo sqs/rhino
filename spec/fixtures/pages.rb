bcdaafe59542a0e65aee795eb6a186aaa6d56ffa
Quinn Slack <qslack@qslack.com>
2008-07-31 20:48:10 +0800

    removed unused fake fixtures script

--- spec/fixtures/pages.rb
+++
@@ -1,7 +0,0 @@
-page_key = 'example.com'
-page_data = {'contents:'=>"<h1>Welcome to Example Page</h1>",
-            'title:'=>'Example Page', 'meta:author'=>'John Smith', 'links:com.example.www/path'=>'Click here'}
-unless page = Page.find(page_key) and page.data == page_data
-  puts "Creating mock Page with key='#{page_key}' and data #{page_data.inspect}"
-  Page.create(page_key, page_data)
-end
